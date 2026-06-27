a8ef2a90181428c85de08446f0de427d8597c5cf
HSIL <163845014+Li5869@users.noreply.github.com>
2026-06-27 21:11:28 +0800

    使用策略模式优化脱敏规则

--- project-backen/blog-common/src/main/java/com/personblog/common/aspect/sensitive/SensitiveStrategy.java
+++ project-backen/blog-common/src/main/java/com/personblog/common/aspect/sensitive/SensitiveStrategy.java
@@ -1,16 +1,10 @@
 package com.personblog.common.aspect.sensitive;
 
 /**
- * 脱敏策略接口：每种敏感数据类型实现一个策略
- * 新增脱敏类型只需实现此接口并注册到 SensitiveAspect 即可
+ * 脱敏策略接口：每种敏感数据类型实现一个策略，只需关心如何脱敏
+ * 字段名→策略的映射由 SensitiveAspect 统一注册管理
  */
 public interface SensitiveStrategy {
-
-    /**
-     * 通过字段名判断是否需要当前策略脱敏
-     * 如字段名含 phone/mobile/tel 则匹配手机号策略
-     */
-    boolean matchesField(String fieldName);
 
     /**
      * 对字段值执行脱敏
